9e2436c08220d92f597f155015ec08e937136df7
Trần Hải Đông <Tranhaidongpk@gmail.com>
2026-01-14 23:28:23 +0700

    add scripts

---
+++ Assets/Game/Scripts/VFX/CoinCollectFX.cs
@@ -0,0 +1,23 @@
+using StarterAssets;
+using UnityEngine;
+
+public class CoinCollectFX : MonoBehaviour
+{
+    ThirdPersonController controller;
+    //[SerializeField] CoinCollectFX;
+
+    private void Start()
+    {
+        controller = GetComponent<ThirdPersonController>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Coin"))
+        {                  
+            
+
+            controller.coinCollected.Invoke();
+        }
+    }
+}
