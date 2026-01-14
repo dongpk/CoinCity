2b0315e58c29f2c5053d34888f068a18cea148bf
Trần Hải Đông <Tranhaidongpk@gmail.com>
2026-01-14 23:26:22 +0700

    add coin logic

---
+++ Assets/Game/Scripts/GamePlay/CoinAnimator.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class CoinAnimator : MonoBehaviour
+{
+    [SerializeField] float argularSpeed = 50f;// tốc độ quay quanh trục Y
+    [SerializeField] float coinHeight = 1f; // chiều cao ban đầu của đồng xu
+    [SerializeField] float movementAmplitude = .5f; // biên độ dao động
+    [SerializeField] float movementFrequency = 1f; // tần số dao động
+    [SerializeField] Transform coinMesh; // tham chiếu đến mesh của đồng xu
+
+    private void Update()
+    {
+        coinMesh.Rotate(0f, argularSpeed * Time.deltaTime, 0f);
+
+        // Tính toán vị trí Y mới dựa trên dao động hình sin
+        float deltaY = movementAmplitude * Mathf.Sin(Time.time * movementFrequency ); 
+
+        coinMesh.localPosition = new Vector3(0f, coinHeight + deltaY, 0f);
+    }
+
+
+}
