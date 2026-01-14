eaca06fde37169ab52a42179dfe978ba1c13d5ae
Trần Hải Đông <Tranhaidongpk@gmail.com>
2026-01-14 23:27:02 +0700

    Add CoinRespawnRandom script for random coin respawn on roads
    Added CoinRespawnRandom.cs to respawn coins at random road locations
    when the player collides with them. The script locates all "Road"-tagged
    GameObjects at Start and moves the coin to a random road's position
    upon player collision. Included the corresponding .meta file.

---
+++ Assets/Game/Scripts/GamePlay/CoinRespawnRandom.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class CoinRespawnRandom : MonoBehaviour
+{
+    private GameObject[] allRoads;
+
+    private void Start()
+    {
+        allRoads = GameObject.FindGameObjectsWithTag("Road");
+        if (allRoads.Length == 0)
+        {
+            Debug.LogWarning("No roads found with the 'Road' tag at Start.");
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            RespawnRandomOnRoads();
+        }
+    }
+    void RespawnRandomOnRoads()
+    {
+       
+
+        if (allRoads.Length == 0 || allRoads==null)
+        {
+            return;
+        }
+        int randomIndex = Random.Range(0, allRoads.Length);
+        GameObject selectedRoad = allRoads[randomIndex];
+
+        Vector3 newPos = selectedRoad.transform.position;
+
+        
+
+        transform.position = newPos;
+    }
+}
