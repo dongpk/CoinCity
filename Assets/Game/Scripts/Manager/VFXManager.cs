7b755b4da6030b95906f19b2175a1f0d744eafa5
Trần Hải Đông <Tranhaidongpk@gmail.com>
2026-01-16 20:07:46 +0700

    crea object pooling for coin vfx

---
+++ Assets/Game/Scripts/Manager/VFXManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class VFXManager : MonoBehaviour
+{
+    public static VFXManager Instance;
+
+    [SerializeField] GameObject coinVfxPrefab;
+    [SerializeField] int poolSize = 10;
+
+    private List<GameObject> poolList = new List<GameObject>();
+
+    private void Awake()
+    {
+        if(Instance== null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        PreparePool();
+    }
+
+    // Tạo và chuẩn bị pool các hiệu ứng VFX
+    void PreparePool()
+    {
+        for(int i=0; i< poolSize; i++)
+        {
+            CreateVFX();    
+        }
+    }
+    GameObject CreateVFX()
+    {
+        GameObject vfx = Instantiate(coinVfxPrefab,transform);
+        vfx.SetActive(false);
+        poolList.Add(vfx);
+        return vfx;
+    }
+
+    public void PlayCoinVFX(Vector3 pos)
+    {
+        GameObject vfxToPlay = null;
+
+        foreach(var vfx in poolList)
+        {
+            if(!vfx.activeInHierarchy)
+            {
+                vfxToPlay = vfx;
+                break;
+            }
+        }
+
+        if(vfxToPlay == null)
+        {
+            vfxToPlay = CreateVFX();
+        }
+
+        vfxToPlay.transform.position = pos;
+        vfxToPlay.SetActive(true);
+
+        ParticleSystem ps = vfxToPlay.GetComponent<ParticleSystem>();
+        if (ps != null) ps.Play();
+    }
+
+}
