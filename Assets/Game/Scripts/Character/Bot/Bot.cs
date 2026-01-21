a7f33da7d5aeaf70d3d93bc5fd12071af9aa40e1
Trần Hải Đông <Tranhaidongpk@gmail.com>
2026-01-21 22:13:30 +0700

    add bot script

--- Assets/Game/Scripts/Character/Bot/Bot.cs
+++ Assets/Game/Scripts/Character/Bot/Bot.cs
@@ -6,9 +6,20 @@ public class Bot : Character
 {
     BotAI aiBot;
     BotConfig configBot;
+ 
 
     public string BotName => configBot != null ? configBot.botName : "Bot";
 
+    protected override void Start()
+    {
+        base.Start();
+        Invoke(nameof(RefreshAnimator), 0.01f);
+    }
+    void RefreshAnimator()
+    {
+        animator = GetComponentInChildren<Animator>();
+
+    }
     public void Initalize(BotConfig botConfig)
     {
         configBot = botConfig;
@@ -18,24 +29,40 @@ public class Bot : Character
 
         aiBot = GetComponent<BotAI>();
         aiBot.SetConfig(configBot);
+        RefreshAnimator();
     }
 
     protected override void Die()
     {
         Debug.Log($"{BotName} hẹo.");
-
+        
+        
+        if (animator != null && animator.gameObject.activeInHierarchy)
+        {
+            animator.SetTrigger("Died");
+        }
         GetComponent<BotAI>().enabled = false;
         GetComponent<UnityEngine.AI.NavMeshAgent>().enabled = false;
+       
+
 
         Invoke(nameof(Respawn), 3f);
     }
-    void Respawn()
+    protected override void Respawn()
     {
         currentHealth = maxHealth;
         GetComponent<BotAI>().enabled = true;
         GetComponent<UnityEngine.AI.NavMeshAgent>().enabled = true;
+        RefreshAnimator();
+        if (animator != null)
+        {
+            animator.Play("Idle Walk Run", 0, 0f);
+        }
+       
         Debug.Log($"{BotName} đã hồi sinh.");
 
         //TODO: làm game manager để respawn bot ở vị trí hợp lý 
     }
+
+   
 }
