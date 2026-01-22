acfacb57e434910332fde81af760dd0452a436eb
Trần Hải Đông <Tranhaidongpk@gmail.com>
2026-01-22 22:06:00 +0700

    Uncomment targetSpeed reset on no movement input

--- Assets/ThirdParty/Models/Starter Assets/Runtime/ThirdPersonController/Scripts/ThirdPersonController.cs
+++ Assets/ThirdParty/Models/Starter Assets/Runtime/ThirdPersonController/Scripts/ThirdPersonController.cs
@@ -225,7 +225,7 @@ namespace StarterAssets
 
             // note: Vector2's == operator uses approximation so is not floating point error prone, and is cheaper than magnitude
             // if there is no input, set the target speed to 0
-            //if (_input.move == Vector2.zero) targetSpeed = 0.0f;
+            if (_input.move == Vector2.zero) targetSpeed = 0.0f;
 
             // a reference to the players current horizontal velocity
             float currentHorizontalSpeed = new Vector3(_controller.velocity.x, 0.0f, _controller.velocity.z).magnitude;
