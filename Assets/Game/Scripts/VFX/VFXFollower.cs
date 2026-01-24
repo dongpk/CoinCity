381ebb8a96e54cfb90abfb28ebe1709e3877feb8
Trần Hải Đông <Tranhaidongpk@gmail.com>
2026-01-24 22:58:33 +0700

    Add VFXFollower script to follow targets with ParticleSystem
    khiến một ParticleSystem bám theo Transform mục tiêu với offset có thể cấu hình. Script sẽ dừng hiệu ứng hạt nếu mục tiêu bị hủy hoặc bằng null, đồng thời xóa tham chiếu mục tiêu khi bị disable.

---
+++ Assets/Game/Scripts/VFX/VFXFollower.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class VFXFollower : MonoBehaviour
+{
+    private Transform target;
+    private Vector3 offset;
+    private ParticleSystem ps;
+
+    private void Awake()
+    {
+        ps = GetComponent<ParticleSystem>();
+    }
+
+    public void SetTarget(Transform newTarget, Vector3 newOffset)
+    {
+        target = newTarget;
+        offset = newOffset;
+    }
+
+    private void LateUpdate()
+    {
+        if (target == null)
+        {
+            // Target bị destroy hoặc null → dừng particle
+            if (ps != null && ps.isPlaying)
+            {
+                ps.Stop();
+            }
+            return;
+        }
+
+        transform.position = target.position + offset;
+    }
+
+    private void OnDisable()
+    {
+        target = null;
+    }
+}
