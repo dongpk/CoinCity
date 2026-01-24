899830b36ff62dc3c187c95abd31fec133f7170b
Trần Hải Đông <Tranhaidongpk@gmail.com>
2026-01-24 22:56:11 +0700

    Add HealthBar script for smooth UI health updates
    HealthBar mới để quản lý thanh máu UI bằng cách sử dụng thành phần Image. Script này cho phép animate mượt mà các thay đổi máu, hỗ trợ cấu hình tốc độ giảm máu và giữ cho thanh máu luôn cố định góc xoay. Đồng thời cung cấp một phương thức để cập nhật thanh máu dựa trên lượng máu hiện tại và máu tối đa. Đã thêm file .meta tương ứng để Unity theo dõi asset.

---
+++ Assets/Game/Scripts/UI/HealthBar.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    [SerializeField] Image healthBarSprite;
+    [SerializeField] float reduceSpeed = 2f;
+    private float targetFill;
+    private Camera cam;
+    private void Start()
+    {
+        cam = Camera.main;
+    }
+    private void Update()
+    {
+        //transform.rotation = Quaternion.LookRotation(transform.position - cam.transform.position,Vector3.up);
+        transform.rotation = Quaternion.identity;
+        healthBarSprite.fillAmount = Mathf.MoveTowards(healthBarSprite.fillAmount,
+                                                        targetFill,
+                                                        reduceSpeed * Time.deltaTime);
+    }
+
+    public void UpdateHealthBar(float maxHealth, float currentHealth)
+    {
+        targetFill= currentHealth / maxHealth;
+    }
+}
