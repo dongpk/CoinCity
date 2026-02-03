a2746883e4ad46ff93c9413b4529ba95c4933f6d
Trần Hải Đông <Tranhaidongpk@gmail.com>
2026-02-03 10:06:51 +0700

    add floating coin text setting

--- Assets/Game/Scripts/UI/HealthBar.cs
+++ Assets/Game/Scripts/UI/HealthBar.cs
@@ -1,10 +1,19 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
+    [Header("Health Bar Settings")]
     [SerializeField] Image healthBarSprite;
     [SerializeField] float reduceSpeed = 2f;
+
+    [Space]
+    [Header("Floating Text Settings")]
+    [SerializeField] GameObject floatingCoinPrefab;
+    [SerializeField] RectTransform coinText;
+
     private float targetFill;
     private Camera cam;
     private void Start()
@@ -23,5 +32,42 @@ public class HealthBar : MonoBehaviour
     public void UpdateHealthBar(float maxHealth, float currentHealth)
     {
         targetFill= currentHealth / maxHealth;
+    }
+    public void ShowCoinGain(int amt, Color color)
+    {
+        Debug.Log("show coin success");
+        if (floatingCoinPrefab == null || coinText == null)
+        {
+            return;
+        }
+        GameObject floatText = Instantiate(floatingCoinPrefab, coinText);
+        floatText.transform.localPosition = Vector3.zero;
+
+        var temp = floatText.GetComponent<TextMeshProUGUI>();
+        if (temp != null)
+        {
+            temp.text = $"+{amt}";
+            temp.color = color;
+        }
+        StartCoroutine(FloatAndDestroy(floatText));
+    }
+    IEnumerator FloatAndDestroy(GameObject obj)
+    {
+        float duration = 1f;
+        float elapsed = 0;
+        RectTransform rectTransform = obj.GetComponent<RectTransform>();
+        Vector3 startPosition = rectTransform.localPosition;
+        var temp = obj.GetComponent<TextMeshProUGUI>();
+        Color startColor = temp.color;
+
+        while (elapsed < duration)
+        {
+            elapsed += Time.deltaTime;
+            float time = elapsed / duration;
+            rectTransform.localPosition = startPosition + Vector3.up * (50 * time);
+            temp.color = new Color(startColor.r, startColor.g, startColor.b, 1f - time);
+            yield return null;
+        }
+        Destroy(obj);
     }
 }
